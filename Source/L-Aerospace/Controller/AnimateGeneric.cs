e43a3e04428d9f89a08a6cac7770a9af212325e4
Lisias T <me@lisias.net>
2026-03-06 22:18:25 -0300

    Some code I found forgotten on the hard disk, but I really don't remember what I was doing last time I edited this file. Looks important tough… :P

--- Source/L-Aerospace/Controller/AnimateGeneric.cs
+++ Source/L-Aerospace/Controller/AnimateGeneric.cs
@@ -18,35 +18,184 @@
 	You should have received a copy of the GNU General Public License 2.0
 	along with L Aerospace. If not, see <https://www.gnu.org/licenses/>.
 */
+using System;
 using System.Collections.Generic;
+#if DEBUG
+using System.Linq;
+#endif
 
 namespace L_Aerospace.Controller.AnimateGeneric
 {
 	public class AnimateGenericController : AbstractController<ModuleAnimateGeneric>
 	{
-		[UI_Toggle (disabledText = "Fixed", scene = UI_Scene.All, enabledText = "Flaperon", affectSymCounterparts = UI_Scene.All)]
-		[KSPField (isPersistant = true, guiActive = false, guiActiveEditor = true)]
-		public bool isFlaperon;
+		[UI_Toggle (disabledText = "Off", scene = UI_Scene.All, enabledText = "On", affectSymCounterparts = UI_Scene.All)]
+		[KSPField (isPersistant = true, guiActive = true, guiActiveEditor = true)]
+		public bool Active;
 
-		[UI_Toggle (disabledText = "Flap", scene = UI_Scene.All, enabledText = "Slat", affectSymCounterparts = UI_Scene.All)]
-		[KSPField (isPersistant = true, guiActive = false, guiActiveEditor = true)]
-		public bool isSlat;
+		[KSPField (isPersistant = true, guiActive = false, guiActiveEditor = false)]
+		protected string AnimationTargetNames = "";
+		protected readonly List<string> animationTargetNames = new List<string>();
+		private readonly List<ModuleAnimateGeneric> animatingTargets = new List<ModuleAnimateGeneric>();
 
-		[UI_Toggle (disabledText = "#autoLOC_6001081", scene = UI_Scene.All, enabledText = "#autoLOC_6001080", affectSymCounterparts = UI_Scene.All)]
-		[KSPField (isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "#autoLOC_6001333")]
-		public bool deploy;
+		[KSPField (isPersistant = true, guiActive = false, guiActiveEditor = false)]
+		protected string TriggerTargetNames = "";
+		protected readonly List<string> triggerTargetNames = new List<string>();
+		private readonly List<ModuleAnimateGeneric> triggerTargets = new List<ModuleAnimateGeneric>();
 
-		[UI_FloatRange (scene = UI_Scene.All, stepIncrement = 0.1f, maxValue = 150f, minValue = -150f, affectSymCounterparts = UI_Scene.All)]
-		[KSPField (guiFormat = "0", isPersistant = true, guiActiveEditor = true, guiActive = true, guiName = "Deploy Angle", guiUnits = "%")]
-		public float deployAngle = 100f;
+		#region PartModule life cycle
+
+		public override void OnAwake()
+		{
+			base.OnAwake();
+			{
+				BaseField bf = this.Fields["Active"];
+				bf.OnValueModified += this.OnActiveModified;
+			}
+		}
+
+		public override void OnLoad(ConfigNode node)
+		{
+			base.OnLoad(node);
+			//{ 
+			//	KSPe.ConfigNodeWithSteroids n = KSPe.ConfigNodeWithSteroids.from(node);
+			//	Log.dbg("OnLoad {0}", n);
+			//	this.animationTargetNames.AddRange(n.GetArrayOf<string>("animationTargetNames"));
+			//	this.triggerTargetNames.AddRange(n.GetArrayOf<string>("triggerTargetNames"));
+			//}
+			{
+				foreach (string s in this.AnimationTargetNames.Split(','))
+					this.animationTargetNames.Add(s.Trim());
+				foreach (string s in this.TriggerTargetNames.Split(','))
+					this.triggerTargetNames.Add(s.Trim());
+			}
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+			base.OnLoad(node);
+			//{ 
+			//	KSPe.ConfigNodeWithSteroids n = KSPe.ConfigNodeWithSteroids.from(node);
+			//	Log.dbg("OnSave {0}", n);
+			//	n.SetArrayOf("animationTargetNames", this.animationTargetNames.ToArray());
+			//	n.SetArrayOf("triggerTargetNames", this.triggerTargetNames.ToArray());
+			//	n.Commit();
+			//}
+		}
 
 		public override void OnStart(StartState state)
 		{
 			if (StartState.None == state) return; // Do nothing on Loading.
 			base.OnStart(state);
+			this.setupMyTargets();
+			this.setupMe();
+		}
 
-			//this.firstSetupMyTargets();
-			//this.setupMe();
+	#endregion
+
+		private void OnActiveModified(object arg1)
+		{
+			this.setupMe();
+		}
+
+		private void setupMe()
+		{
+			if (this.Active)	this.updateDelegate = this.startAnimations;
+			else				this.updateDelegate = this.finishAnimations;
+		}
+
+		private void setupMyTargets()
+		{
+			this.build(this.animationTargetNames, this.animatingTargets);
+			this.build(this.triggerTargetNames, this.triggerTargets);
+
+			for (int i = 0 ; i < this.targets.Count; ++i)
+			{
+				{ 
+					BaseField bf = this.targets[i].Fields["status"];
+					bf.guiActive = false;
+					bf.guiActiveEditor = false;
+				}
+				{
+					BaseEvent a = this.targets[i].Events["Toggle"];
+					a.guiActive = false;
+					a.guiActiveEditor = false;
+				}
+			}
+
+			for (int i = 0 ; i < this.animatingTargets.Count; ++i)
+			{
+				this.targets[i].isOneShot = false;
+			}
+
+			for (int i = 0 ; i < this.triggerTargets.Count; ++i)
+			{
+
+			}
+		}
+
+		private void build(List<string> targetNames, List<ModuleAnimateGeneric> targets)
+		{
+			targets.Clear();
+			foreach (ModuleAnimateGeneric m in this.targets) if (targetNames.Contains(m.animationName))
+				targets.Add(m);
+		#if DEBUG
+			Log.dbg("build {0} {1}", string.Join(",", targetNames.ToArray()), string.Join(",", (from t in targets select t.animationName).ToArray()));
+		#endif
+		}
+
+		private void startAnimations()
+		{
+			Log.dbg("startAnimations");
+			for (int i = 0 ; i < this.triggerTargets.Count; ++i)
+			{
+				ModuleAnimateGeneric m = this.targets[i];
+				if (!m.enabled) continue;
+				if (m.IsMoving()) continue;
+				if (1 == m.animTime) continue;
+				this.ExecuteAsCoroutine(() => m.Toggle() );
+			}
+
+			for (int i = 0 ; i < this.animatingTargets.Count; ++i)
+			{
+				ModuleAnimateGeneric m = this.targets[i];
+				if (!m.enabled) continue;
+				if (m.IsMoving()) continue;
+				this.ExecuteAsCoroutine(() => m.Toggle() );
+			}
+
+			this.updateDelegate = this.updateAnimation;
+		}
+
+		private void updateAnimation()
+		{
+			Log.dbg("updateAnimation");
+			for (int i = 0 ; i < this.animatingTargets.Count; ++i)
+			{
+				ModuleAnimateGeneric m = this.targets[i];
+				Log.dbg("updateAnimation {0} {1} {2}", m.name, m.enabled, m.aniState);
+				if (!m.enabled) continue;
+				if (ModuleAnimateGeneric.animationStates.MOVING == m.aniState) continue;
+				if (1.0 == m.animTime)
+				{
+					m.animSpeed = Math.Abs(m.animSpeed);
+					this.ExecuteAsCoroutine(() => m.Toggle() );
+				}
+			}
+		}
+
+		private void finishAnimations()
+		{
+			Log.dbg("finishAnimations");
+			for (int i = 0 ; i < this.triggerTargets.Count; ++i)
+			{
+				ModuleAnimateGeneric m = this.targets[i];
+				if (!m.enabled) continue;
+				if (0 == m.animTime) continue;
+				if (ModuleAnimateGeneric.animationStates.MOVING == m.aniState) continue;
+				this.ExecuteAsCoroutine(() => m.Toggle() );
+			}
+
+			this.updateDelegate = this.dummyUpdate;
 		}
 	}
 }
---
+++ Source/Tests/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Tests
+{
+	class MainClass
+	{
+		private static string FormatToUserInterface(float value, int decimals, string unit = null)
+		{
+			Console.WriteLine("{0} {1} {2}", value, decimals, unit);
+			{
+				string repr = ((int)value).ToString();
+				int exponent = (int)(repr.Length -1);
+				switch (exponent)
+				{
+					case 0: case 1: case 2:
+						break;
+					case 3: case 4: case 5:
+						unit = "k" + unit??"";
+						decimals = 2;
+						value /= (float)Math.Pow(10, 3);
+						break;
+					case 6: case 7: case 8:
+						unit = "m" + unit??"";
+						decimals = 4;
+						value /= (float)Math.Pow(10, 6);
+						break;
+					default:
+						decimals = 6;
+						--exponent;
+						value /= (float)Math.Pow(10, exponent);
+						unit = string.Format("e+{0}", exponent) + unit??"";
+						break;
+				}
+				Console.WriteLine("{0} {1} {2}", repr, exponent, value);
+			}
+			string mask = "{0:0" + (0 != decimals?"." + new string('0', decimals):"") + "}" + unit??"";
+			Console.WriteLine("{0} {1} {2}", mask, value, String.Format(mask, value));
+			return string.Format(mask, value);
+		}
+
+		private static double ResourceOnus(double x)
+		{
+			// y=a*log_base_1.5(x-h)+k https://www.desmos.com/calculator
+			double thisResourceOnus = Math.Max(1, 0.3 * Math.Log(x - -4.06, 1.5) + -0.2);
+			return thisResourceOnus;
+		}
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("\n{0}\n---", FormatToUserInterface(99.32f, 2, "oC"));
+			Console.WriteLine("\n{0}\n---", FormatToUserInterface(340.23f, 2, "oC"));
+			Console.WriteLine("\n{0}\n---", FormatToUserInterface(1340.3f, 2, "oC"));
+			Console.WriteLine("\n{0}\n---", FormatToUserInterface(102388.233f, 2, "oC"));
+			Console.WriteLine("\n{0}\n---", FormatToUserInterface(10233828.233f, 2, "oC"));
+
+			for (int i = 0 ; i < 100; ++i)
+			Console.WriteLine("{0} : {1}", i, ResourceOnus((double)i));
+		}
+	}
+}
